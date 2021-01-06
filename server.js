c9a70907d886a5cccbfa418a07d564fb0edea89b
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2021-01-06 09:25:40 -0700

    added directory folders and created burgerControllers.js file. Also finished server.js file

--- server.js
+++ server.js
@@ -21,3 +21,9 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars')
 
 //import routes and give the server access to them
+const routes = require('./controllers/burgerControllers');
+
+app.use(routes);
+
+//start server to begin listening to end user requests
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
