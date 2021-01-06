63f1d6b63459e07a40e0333a4ed0a11c08108699
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2021-01-06 09:20:42 -0700

    setting up server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+//dependencies
+const express = require('express');
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//setting up express app
+const app = express();
+
+//serves static content for the app from the "public" directory in the application directory
+app.use(express.static('public'));
+
+//parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set up handlebars
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
+
+//import routes and give the server access to them
